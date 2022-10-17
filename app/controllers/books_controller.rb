4d9d8975cbf53e9a8ef4105b5207280e7bd9ff20
Madalina Muresan <madalina.muresan@hotmail.com>
2022-10-17 11:21:39 +0300

    Add show action for books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     render json: Book.all, each_serializer: BookSerializer
   end
+
+  def show
+    render json: Book.find(params[:id]), serializer: BookSerializer
+  end
 end
