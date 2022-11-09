1107eff4cce0b669e507f06cffc9f96c566972aa
Madalina Muresan <madalina.muresan@hotmail.com>
2022-11-09 09:16:38 +0200

    Add pagination method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include Pagy::Backend
+
+  def paginate_items(items)
+    pagy, paginated_item = pagy(items)
+    pagy_headers_merge(pagy) if Pagy
+    
+    paginated_item
+  end
 end
