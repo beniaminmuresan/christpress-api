024170bdce602bd9ae1569c7faf9d2327584783b
Beniamin Muresan <beniamin.muresan@fabelx.com>
2022-12-14 06:13:56 +0200

    Improve search by song line

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,12 @@ class Song < ApplicationRecord
 
   validates_presence_of :name
   has_many :song_parts, dependent: :destroy
+  has_many :song_lines, through: :song_parts
 
   scope :by_keyword, lambda { |keyword|
-    where('songs.name ILIKE ?', "%#{keyword}%") 
+    joins(:song_lines)
+      .where('songs.name ILIKE ?', "%#{keyword}%")
+      .or(where('song_lines.value ILIKE ?', "%#{keyword}%"))
+      .distinct
   }
 end
